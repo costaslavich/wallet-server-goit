6858232369bc4bdc005f3879dafd68af247efb1f
Konstantin Chernykh <costaslavich@gmail.com>
2019-10-04 11:07:21 +0300

    hw-04

---
+++ HW-04/config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  jwtSecretKey: "secret_key",
+  PORT: process.env.PORT || 5000,
+  mongodb_URI:
+    "mongodb+srv://wallet:password34GH@wallet-xn0g4.mongodb.net/wallet?retryWrites=true&w=majority"
+};
